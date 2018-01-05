37b13f959a7b456f5aea29972df35aaa27a558a7
Adam Bihari <adam.bihari@gmail.com>
2018-01-06 01:04:51 +0200

    Cache git passwords

--- inst_scripts/inst_git.sh
+++ inst_scripts/inst_git.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 git config --global user.email "adam.bihari@gmail.com"
 git config --global user.name "Adam Bihari"
+git config --global credentail.helper cache
+git config --global credentail.helper 'cache --timeout=3600'
