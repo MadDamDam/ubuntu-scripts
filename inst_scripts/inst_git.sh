ebdb7dca907070546cbd241daf12a03c9f7fb0db
Adam Bihari <adam.bihari@gmail.com>
2018-01-06 01:06:27 +0200

    Remove push.default message

--- inst_scripts/inst_git.sh
+++ inst_scripts/inst_git.sh
@@ -3,3 +3,4 @@ git config --global user.email "adam.bihari@gmail.com"
 git config --global user.name "Adam Bihari"
 git config --global credentail.helper cache
 git config --global credentail.helper 'cache --timeout=3600'
+git config --global push.default simple
