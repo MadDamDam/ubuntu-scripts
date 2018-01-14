03a47d852a9b98876b4e0f1880f14030d5258962
Adam Bihari <adam.bihari@gmail.com>
2018-01-14 15:36:16 +0200

    Add Atom install

--- inst_scripts/inst_apt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt install curl -y
-sudo apt install git -y
---
+++ inst_scripts/inst_atom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt install curl -y
+sudo apt install git -y
+curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update
+sudo apt-get install atom -y
