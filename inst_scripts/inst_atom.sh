a70e58862bf0040b2dce5c060f887f5e8e16d1ac
MadDamDam <adam.bihari@gmail.com>
2018-03-20 14:31:24 +0200

    Update inst_atom.sh

--- inst_scripts/inst_atom.sh
+++ inst_scripts/inst_atom.sh
@@ -5,4 +5,5 @@ sudo apt install git -y
 curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update
+sudo apt-get install terminator -y
 sudo apt-get install atom -y
