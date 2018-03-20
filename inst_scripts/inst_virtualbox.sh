5668afcb2cd1734fa43ab6f3b4b513661e4bb32b
Adam Bihari <adam.bihari@gmail.com>
2018-03-20 18:21:10 +0200

    Add minikube

---
+++ inst_scripts/inst_virtualbox.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "deb https://download.virtualbox.org/virtualbox/debian xenial contrib" | sudo tee -a /etc/apt/sources.list
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install virtualbox-5.2 -y
+
