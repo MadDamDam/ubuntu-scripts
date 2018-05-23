05e6b5c9172af8a529a13277e9734ec4fc3bb3b4
MadDamDam <adam.bihari@gmail.com>
2018-05-23 20:50:35 +0300

    Update inst_zsh.sh

--- inst_scripts/inst_zsh.sh
+++ inst_scripts/inst_zsh.sh
@@ -5,3 +5,23 @@ sudo apt-get install zsh
 zsh --version
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+: '
+  git
+  ruby
+  history-substring-search
+  github
+  pip
+  python
+  rake
+  chucknorris
+  command-not-found
+  catimg
+  dirhistory
+  docker
+  lol
+  rand-quote
+  sudo
+  vagrant
+  web-search
+  wd
+'
