f3f8f37f53c39570768a9de15d0ee9a1e5d9abd5
MadDamDam <adam.bihari@gmail.com>
2018-10-04 20:58:37 +0300

    Update inst_mongo_4_0_on_xenial.sh

--- inst_scripts/inst_mongo_4_0_on_xenial.sh
+++ inst_scripts/inst_mongo_4_0_on_xenial.sh
@@ -3,3 +3,4 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD7
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org=4.0.2 mongodb-org-server=4.0.2 mongodb-org-shell=4.0.2 mongodb-org-mongos=4.0.2 mongodb-org-tools=4.0.2
+sudo service mongod start
